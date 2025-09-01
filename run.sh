2ff474c4d1f7817917299ddf0bd9d20d3fa881c7
Adam Smith <asmith607@gmail.com>
2025-09-01 07:05:26 -0400

    more runtime tweaking

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ compile-assets.sh
 find ./content -type f \( -iname \*.jpg -o -iname \*.png \) -size +6M -exec du -h '{}' + | sort -hr | head -10
 # hugo server "${1}" --navigateToChanged 
 
-# hugo server "${1}" --cleanDestinationDir --gc --disableFastRender --noHTTPCache --navigateToChanged --logLevel info 
-hugo server "${1}" --noHTTPCache --navigateToChanged 
+HUGO_CACHEDIR=/run/user/$(id -u)/hugo_cache
+
+# hugo server "${1}" --cleanDestinationDir --gc --disableFastRender=false --noHTTPCache --navigateToChanged --logLevel info 
+hugo server "${1}" --noHTTPCache --disableFastRender=false --navigateToChanged 
 
 # for large projects, increase number of inotify watchers when running server:
 # echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
